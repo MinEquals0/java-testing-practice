eeb44f816789ee5f16f11b13924bbbf9e1e586e0
MinEquals0 <minequals0@gmail.com>
2025-05-22 16:22:29 +0900

    feat: create BeforeEach1Test

--- src/test/java/com/minequals0/basic/BeforeEach1Test.java
+++ src/test/java/com/minequals0/basic/BeforeEach1Test.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+
 public class BeforeEach1Test {
 
+    // 테스트 케이스가 실행 시 @BeforeEach 붙인 메서드가 먼저 실행되므로
+    // 객체나 값에 대한 초기화 작업등 전처리 과정에 사용 가능
     @BeforeEach
-    public void init() {
+    public void init(){
         System.out.println("Pre-processing before each test case");
     }
 
@@ -19,7 +22,7 @@ public class BeforeEach1Test {
 
     @DisplayName("@BeforeEach Test2")
     @Test
-    public void beforeEachTest2() {
+    public void beforeEachTest2(){
 
     }
 }
