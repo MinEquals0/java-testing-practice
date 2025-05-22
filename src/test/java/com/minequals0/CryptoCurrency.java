0f0da447a0bbd1462e0fec449d6358b6419ba38e
MinEquals0 <minequals0@gmail.com>
2025-05-22 16:02:56 +0900

    chore: add a comment for better understanding

--- src/test/java/com/minequals0/CryptoCurrency.java
+++ src/test/java/com/minequals0/CryptoCurrency.java
@@ -3,6 +3,8 @@ package com.minequals0;
 import java.util.HashMap;
 import java.util.Map;
 
+
+// 테스트를 위한 클래스
 public class CryptoCurrency {
     private String currencyName;
     private String currencyUnit;
