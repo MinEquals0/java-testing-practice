32327c49f950354978dd5d32d57be579fa43a073
MinEquals0 <minequals0@gmail.com>
2025-05-22 15:55:02 +0900

    chore: add comments for better understanding

--- src/test/java/com/minequals0/basic/HelloJunitTest.java
+++ src/test/java/com/minequals0/basic/HelloJunitTest.java
@@ -1,5 +1,6 @@
 package com.minequals0.basic;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,9 +11,14 @@ public class HelloJunitTest {
     @DisplayName("Hello Junit Test")
     @Test
     public void assertionTest1() {
-        String actual = "Hello, JUnit";
         String expected = "Hello, JUnit";
+        // String expected = "Hello, World";
+        String actual = "Hello, JUnit";
 
+        // 기대값과 실제 결과값이 같은지 검증
         assertEquals(expected, actual);
+
+        // 위와 같은 코드
+        // Assertions.assertEquals(expected, actual);
     }
 }
