e2719a659dcfc560988cf51071006844970914bb
MinEquals0 <minequals0@gmail.com>
2025-05-22 17:27:35 +0900

    feat: AssumptionTest

--- src/test/java/com/minequals0/basic/AssumptionTest.java
+++ src/test/java/com/minequals0/basic/AssumptionTest.java
@@ -6,17 +6,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+// Assumption : 특정 환경에만 테스트 케이스가 실행되도록 함
 public class AssumptionTest {
     @DisplayName("Assumption Test")
     @Test
-    public void assumptionTest() {
-        assumeTrue(System.getProperty("os.name").startsWith("Windows"));
-//        assumeTrue(System.getProperty("os.name").startsWith("Linux"));
+    public void assumptionTest(){
+        // 파라미터 값이 true면 아래 로직들 실행
+        // 테스트 케이스를 실행하는 PC 운영체제가 Windows인가? T면 아래 로직 실행됨
+         assumeTrue(System.getProperty("os.name").startsWith("Windows"));
+
+        // 운영체제가 리눅스 일 때, 결과값 ignored 아래 코드 실행 안됨
+        // assumeTrue(System.getProperty("os.name").startsWith("Linux"));
+
         System.out.println("execute?");
-        assertTrue(processOnlyWindowsTask());
+        assumeTrue(processOnlyWindowTask());
     }
 
-    private boolean processOnlyWindowsTask() {
+    private boolean processOnlyWindowTask() {
         return true;
     }
 }
